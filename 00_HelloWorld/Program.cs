7e6bb4430031fcc95d24f444422aeb38b12c10a7
Spawlak93 <spawlak1993@gmail.com>
2021-12-10 07:59:11 -0600

    Initial

---
+++ 00_HelloWorld/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("My Name is Simon");
+
+for(int i = 0; i < 10; i++)
+{
+    System.Console.WriteLine(i);
+}
